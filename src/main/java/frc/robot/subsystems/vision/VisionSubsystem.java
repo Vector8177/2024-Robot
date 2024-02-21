fe1de4378c999e51dd8d8cc5fd1bef7b39150b6f
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-02-21 14:37:33 -0600

    revamped Vision Subsystem

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -1,44 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.vision;
 
+import org.photonvision.PhotonCamera;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public  class VIsionSubsystem extends SubsystemBase
-{
-    
-    // With eager singleton initialization, any static variables/fields used in the 
-    // constructor must appear before the "INSTANCE" variable so that they are initialized 
-    // before the constructor is called when the "INSTANCE" variable initializes.
-    
-/**
- * The Singleton instance of this VIsionSubsystem. Code should use
- * the {@link #getInstance()} method to get the single instance (rather
- * than trying to construct an instance of this class.)
- */
-private final static VIsionSubsystem INSTANCE = new VIsionSubsystem();
+public class VisionSubsystem extends SubsystemBase {
+  private PhotonCamera photonCamera;
+  /** Creates a new VisionSubsystem. */
+  public VisionSubsystem() {
+    //name may not be vectorcam (can update later)
+    photonCamera = new PhotonCamera("vectorcam");
+  }
 
-/**
- * Returns the Singleton instance of this VIsionSubsystem. This static method 
- * should be used, rather than the constructor, to get the single instance
- * of this class. For example: {@code VIsionSubsystem.getInstance();}
- */
-@SuppressWarnings("WeakerAccess")
-public static VIsionSubsystem getInstance()
-{    
-    return INSTANCE;
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
-
-    /**
-     * Creates a new instance of this VIsionSubsystem. This constructor
-     * is private since this class is a Singleton. Code should use
-     * the {@link #getInstance()} method to get the singleton instance.
-     */
-private VIsionSubsystem()
-    {
-        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
-        //       in the constructor or in the robot coordination class, such as RobotContainer.
-        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
-        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
-    }
-}
-
