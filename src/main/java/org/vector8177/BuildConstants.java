6e2e33d3a3890f0ee018021760f39ca04d4c2621
Ishaan Masil <61901829+StarkOdinson612@users.noreply.github.com>
2024-03-18 15:59:40 -0500

    Fixed ADAstarAK pathfidning algo

--- src/main/java/org/vector8177/BuildConstants.java
+++ src/main/java/org/vector8177/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 109;
-  public static final String GIT_SHA = "d7e77869e9b24b0f6c1ff6ff4973a844f50c3fd6";
-  public static final String GIT_DATE = "2024-03-17 16:21:14 EDT";
+  public static final int GIT_REVISION = 110;
+  public static final String GIT_SHA = "350b7710aafdb74c47bd49ac917bc9ac2166b2f1";
+  public static final String GIT_DATE = "2024-03-17 17:45:51 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-17 17:23:55 EDT";
-  public static final long BUILD_UNIX_TIME = 1710710635576L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-03-18 16:56:05 EDT";
+  public static final long BUILD_UNIX_TIME = 1710795365998L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
